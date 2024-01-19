9ded007265ec4e346614f180c6e10c7dbfc4d2d5
GiangNguyen28-glicth <giangnguyen3246>
2024-01-19 17:18:47 +0700

    feat: core

--- libs/clients/mongoose/src/mongoose.config.ts
+++ libs/clients/mongoose/src/mongoose.config.ts
@@ -12,6 +12,11 @@ export class MongooseConfig extends ClientConfig {
     super(props);
     this.host = props.host ?? 'localhost';
     this.port = toInt(props.port, 27017);
+    this.uri = props.uri;
+    this.username = props.username;
+    this.password = props.password;
+    this.database = props.database;
+    this.port = props.port;
   }
 }
 
@@ -28,6 +33,6 @@ export class MongooseClientConfig extends AbstractClientConfig<MongooseConfig> {
     if (this.getConfig.uri) {
       return this.getConfig.uri;
     }
-    return `mongodb://$${this.getConfig.username}:${this.getConfig.password}@${this.getConfig.host}:${this.getConfig.port}/${this.getConfig.database}?authSource=admin`;
+    return `mongodb://${this.getConfig.username}:${this.getConfig.password}@${this.getConfig.host}:${this.getConfig.port}/${this.getConfig.database}?authSource=admin`;
   }
 }
